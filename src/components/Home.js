0ac8a395f009b94f767fb4748e88743f616f851d
Michael Nugent <NugentMichael@live.com>
2022-03-05 16:00:21 -0800

    Home instructions

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,11 +1,27 @@
-const Home = () => {
-  return <header>
-      <title>N-Queens EA</title>
-      <h1>N-Queens</h1>
-      <h2>Solved by Evolutionary Algorithm</h2>
+import queenChessPiece from '../images/queen.png';
+import { Link } from 'react-router-dom'
 
-      <p>Use the "Options" panel to set the profile of the experiment.  Click run from that page to see all the results.  Select a result to see what it looks like on a chess board.</p>
-  </header>;
+const Home = () => {
+  return <>
+    <title>N-Queens EA</title>
+    <h1>N-Queens</h1>
+    <img src={queenChessPiece} alt="Queen chess piece" id="homeQueen" />
+    <h2>Solved by Evolutionary Algorithm</h2>
+
+    <h3>Instructions</h3>
+    <p>
+      <ol>
+        <li>Use the <Link to="/options">"Options" page</Link> to set the parameters of the experiment.
+        </li>
+        <li>Click run to see the final population set ordered by permutation score.</li>
+        <li>Select a member of the final population set to see what the queen positions look like on a chess board.</li>
+        <li>
+          Go back to the <Link to="/options">"Options" page</Link> anytime to run another experiment.
+        </li>
+      </ol>
+    </p>
+
+  </>;
 };
 
 export default Home;
