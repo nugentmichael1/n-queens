8e0b907c8d600272fc7a24dc841c46096d7be5c7
Michael Nugent <NugentMichael@live.com>
2022-03-05 16:10:27 -0800

    about section content

--- src/components/About.js
+++ src/components/About.js
@@ -2,7 +2,7 @@
 const About = () => {
 
     return <><h1>About</h1>
-    <p>A CSCI 191T - Bio-Inspired Machine Learning project from Spring 2022 at CSU, Fresno, and taught by Professor Athanasios Aris (Thanos) Panagopoulos.</p>
+    <p>A project of <a href="https://csm.fresnostate.edu/csci/index.html">CSU, Fresno's Computer Science </a>  Bio-Inspired Machine Learning course (CSCI 191T) taught by Professor Athanasios Aris (Thanos) Panagopoulos.  Spring 2022.</p>
     </>
 };
 
