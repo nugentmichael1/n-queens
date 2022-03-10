a89380efa01447df5c2ac7a79d1c31ace9dc4287
Michael Nugent <NugentMichael@live.com>
2022-03-10 00:23:19 -0800

    link colors

--- src/components/About.js
+++ src/components/About.js
@@ -3,7 +3,7 @@ const About = () => {
 
     return <>
         {/* <h1>About</h1> */}
-        <p>A project of <a href="https://csm.fresnostate.edu/csci/index.html">CSU, Fresno's Computer Science </a>  Bio-Inspired Machine Learning course (CSCI 191T) taught by Professor Athanasios Aris (Thanos) Panagopoulos.  Spring 2022.</p>
+        <p>A <a href="https://www.fresnostate.edu/" className="fs">CSU, Fresno</a> <a href="https://csm.fresnostate.edu/csci/index.html" className="fs">Computer Science Program</a> project from Professor Athanasios Aris (Thanos) Panagopoulos' 2022 Bio-Inspired Machine Learning course (CSCI 191T).</p>
         <h2>Group Members</h2>
         <ul>
             <li>Leon Kantikov</li>
